7d0c6eecb8920f44732ce24416e1cbd3bc6073d2
Navdeep <beenavdeep@gmail.com>
2021-04-21 14:15:58 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,47 +1,19 @@
 import './App.css';
-import restaurant from './restaurant.jpeg'
 
-const dishes = ["Mac and cheese", "Pizza", "Burger", "Coffee(Hot and cold)", "Tea"];
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  );
+function SecretComponent() {
+  return <h1>Secret info for authorized users only.</h1>
 }
 
-function Main(props) {
-  return (
-    <section>
-      <p>We serve the most {props.adjective} food around</p>
-      <img src={restaurant} alt="actual restaurant pic"></img>
-      <ul style={{textAlign:"left"}}>
-        {props.menuItems.map((item) => (
-          <li key={item.index}>{item.title}</li>
-        ))}
-      </ul>
-    </section>
-  );
+function RegularComponent() {
+  return <h1>Everyone can see this.</h1>
 }
 
-function Footer(props) {
-  return (
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  );
-}
-function App() {
-  var currYear = new Date().getFullYear();
-  var dishesObj = dishes.map((dishName, i) => ({index:i, title: dishName}));
-
-  return (
-    <div className="App">
-      <Header name="Navdeep"/>
-      <Main adjective= "amazing" menuItems={dishesObj}/>
-      <Footer year={currYear}/>
-    </div>
-  );
+function App(props) {
+    return (
+      <> //react fragment
+        {props.authorized ? <SecretComponent /> : <RegularComponent />}
+      </>
+    );
 }
 
 export default App;
