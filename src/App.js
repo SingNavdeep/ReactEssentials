baf7b4c916fc1aa2786a124cad44d53405e8f561
Navdeep <beenavdeep@gmail.com>
2021-04-21 13:15:35 +0530

    react essentials.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import restaurant from './restaurant.jpeg'
 
+const dishes = ["Mac and cheese", "Pizza", "Burger", "Coffee(Hot and cold)", "Tea"];
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  );
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>We serve the most {props.adjective} food around</p>
+      <img src={restaurant} alt="actual restaurant pic"></img>
+      <ul style={{textAlign:"left"}}>
+        {props.menuItems.map((item) => (
+          <li key={item.index}>{item.title}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  );
+}
 function App() {
+  var currYear = new Date().getFullYear();
+  var dishesObj = dishes.map((dishName, i) => ({index:i, title: dishName}));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Navdeep"/>
+      <Main adjective= "amazing" menuItems={dishesObj}/>
+      <Footer year={currYear}/>
     </div>
   );
 }
