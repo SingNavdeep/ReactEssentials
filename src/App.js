e0d02626e8a6414ca5bb0d7dbc2d3c9cde6f447a
Navdeep <beenavdeep@gmail.com>
2021-04-24 17:53:15 +0530

    useReducer example

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
-import {useState, useEffect} from "react";
+import {useReducer} from "react";
 
 function App() {
-  const [checked, setChecked] = useState(false);
+  const [checked, toggle] = useReducer(
+    checked => !checked,
+    false);
   return (
     <>
     {/*
           when new state is dependent on prev state,
-          pass a function to setChecked 
+          pass a function to setChecked.
+          The below arrow function for onChange callback can be moved to another function(toggle in this case) with reducer
         */}
       <input 
         type = "checkbox" value = {checked}
-        onChange = {() => setChecked(checked => !checked) } 
+        onChange = {toggle} 
       />
       <p>{checked ? "checked" : "unchecked"}</p>
     </>
