de82e5ad181f91ed03d9a8aed33a7b1b447c88aa
Navdeep <beenavdeep@gmail.com>
2021-04-24 17:41:00 +0530

    reducer hook setup.

--- src/App.js
+++ src/App.js
@@ -2,30 +2,18 @@ import './App.css';
 import {useState, useEffect} from "react";
 
 function App() {
-  const [emotion, setEmotion] = useState("Happy");
-
-  function changeEmotion(e) {
-    //console.log(`Current emotion is ${e.target.innerText}`);
-    setEmotion(e.target.innerText);
-  }
-  function logEffect() {
-    console.log(`Selected emotion is ${emotion}`);
-  }
-  /*
-    useEffect is a hook that is called everytime after a component is rendered or re-rendered
-    It takes a callback function.
-  */
-  useEffect(logEffect);
-
+  const [checked, setChecked] = useState(false);
   return (
     <>
-      <h1>Current emotion is {emotion}.</h1>
-      {/*
-        onclick takes a callback function. we can also define that function inline.
-      */}
-      <button name = "happy" onClick = {changeEmotion}>Happy</button>
-      <button name = "frustrate" onClick = {(e) => setEmotion(e.target.innerText)}>Frustrated</button>
-      <button name = "enthuse" onClick = {changeEmotion}>Enthusiastic</button>
+    {/*
+          when new state is dependent on prev state,
+          pass a function to setChecked 
+        */}
+      <input 
+        type = "checkbox" value = {checked}
+        onChange = {() => setChecked(checked => !checked) } 
+      />
+      <p>{checked ? "checked" : "unchecked"}</p>
     </>
   );
 }
