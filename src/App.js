c127c5f4d1fecd30b46f88625f3522a5117ea69c
Navdeep <beenavdeep@gmail.com>
2021-04-25 18:38:07 +0530

    using hooks to display data from an API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import './App.css';
-import {useReducer} from "react";
+import {useEffect, useState} from "react";
 
-function App() {
-  const [checked, toggle] = useReducer(
-    checked => !checked,
-    false);
-  return (
-    <>
-    {/*
-          when new state is dependent on prev state,
-          pass a function to setChecked.
-          The below arrow function for onChange callback can be moved to another function(toggle in this case) with reducer
-        */}
-      <input 
-        type = "checkbox" value = {checked}
-        onChange = {toggle} 
-      />
-      <p>{checked ? "checked" : "unchecked"}</p>
-    </>
-  );
+function App({login}) {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    console.log(setUserData);
+    fetch(`https://api.github.com/users/${login}`)
+    .then((response) => response.json())
+    .then(setUserData);
+  }, []);
+
+  if(userData) {
+    return <div>{JSON.stringify(userData)}</div>
+  }
+    return <div>No user data</div>
 }
 
 export default App;
